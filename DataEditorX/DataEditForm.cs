00cdb583b87cb4ca6281b2a7c9e07a84e8bbc15a
Yuniko-n <3038572935@qq.com>
2020-07-16 10:35:23 +0800

    version

--- DataEditorX/DataEditForm.cs
+++ DataEditorX/DataEditForm.cs
@@ -1109,7 +1109,7 @@ namespace DataEditorX
 			MyMsg.Show(
 				LanguageHelper.GetMsg(LMSG.About) + "\t" + Application.ProductName + "\n"
 				+ LanguageHelper.GetMsg(LMSG.Version) + "\t" + Application.ProductVersion + "\n"
-				+ LanguageHelper.GetMsg(LMSG.Author) + "\t菜菜 | Nanahira | JoyJ | Yuniko-n");
+				+ LanguageHelper.GetMsg(LMSG.Author) + "\tYuniko-n");
 		}
 
 		void Menuitem_checkupdateClick(object sender, EventArgs e)
