0c5973d7f7c1b514815a4f01e3f87979f9978070
Yuniko-n <3038572935@qq.com>
2021-03-02 13:06:23 +0800

    自动解压更新

--- DataEditorX/Core/TaskHelper.cs
+++ DataEditorX/Core/TaskHelper.cs
@@ -169,7 +169,7 @@ namespace DataEditorX.Core
                 MyPath.Combine(Application.StartupPath, newver + ".zip")))
             {
                 MyMsg.Show(LMSG.DownloadSucceed);
-				System.Diagnostics.Process.Start("explorer.exe", Application.StartupPath);
+				System.Diagnostics.Process.Start("DataEditorX-Upgrade.exe", newver + ".zip");
             }
             else
             {
