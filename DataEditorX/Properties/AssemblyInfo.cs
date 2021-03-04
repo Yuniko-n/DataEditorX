acfe989f1a189215d3eab4b332170a1c1eabcc08
Yuniko-n <3038572935@qq.com>
2021-03-04 16:17:13 +0800

    version

--- DataEditorX/Properties/AssemblyInfo.cs
+++ DataEditorX/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DataEditorX")]
-[assembly: AssemblyCopyright("Copyright 2020")]
+[assembly: AssemblyCopyright("Copyright 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.7")]
+[assembly: AssemblyVersion("3.0.0.8")]
